4613d9d5a1e1d756723ad78a83baa50ec1161678
chemicalchild <78377717+chemicalchild@users.noreply.github.com>
2023-12-07 22:25:13 +0100

    Update Model.py

--- fuelmonitoring/Model.py
+++ fuelmonitoring/Model.py
@@ -6,10 +6,12 @@ class LocationFuelData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+    altitude = db.Column(db.Float, nullable=False)
     fuel_level = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __init__(self, latitude, longitude, fuel_level):
+    def __init__(self, latitude, longitude, altitude, fuel_level):
         self.latitude = latitude
         self.longitude = longitude
+        self.altitude = altitude
         self.fuel_level = fuel_level
